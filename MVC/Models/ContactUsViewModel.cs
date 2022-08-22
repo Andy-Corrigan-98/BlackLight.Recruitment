b93f9f32dd1df2f357fa55459ec767f7220e0b8b
admin <admin@ASUS-LAPTOP>
2022-08-22 11:15:35 +0100

    Add project files.

---
+++ MVC/Models/ContactUsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlackLight.Recruitment.MVC.Models
+{
+    public class ContactUsViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")] //No need for Regex to check for an @ since there's built-in email validation
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
---
+++ UnitTests/Algorithms.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackLight.Recruitment.UnitTests
+{
+    class Algorithms
+    {
+        public static string ReverseWordsInString(string input)
+        {
+
+            return string.Join(" ", input.Split(' ').Reverse());
+        }
+
+        public static string ReverseWordsInStringWithoutUsingAnyLinqMethods(string input)
+        {
+            var words = input.Split(' ');
+            var reversedWords = new List<string>();
+            for (var i = words.Length - 1; i >= 0; i--)
+            {
+                reversedWords.Add(words[i]);
+            }
+
+            return string.Join(" ", reversedWords);
+        }
+
+        internal static int[] SortWithoutUsingBuiltInSortMethods(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        int temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+    }
+}
